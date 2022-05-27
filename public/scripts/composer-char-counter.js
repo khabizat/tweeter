cf3e6195bca91f3431e14fb00c47bd806a60f568
Khabizat Sharapudinova <habizat22@gmail.com>
2022-05-27 02:47:42 +0000

    Delete comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
     let countLength = $(this).val().length;
     let output = 140;
     let remainingChars = output - countLength;
-    //save to a variable and reuse on lines 9 and 11
-    // $(this).parent().children('div').children('output').text(remainingChars);
     if (remainingChars < 0) {
       $(this).parent().children('div').children('output').text(remainingChars).removeClass("output").addClass("negativeCount");
     } else {
